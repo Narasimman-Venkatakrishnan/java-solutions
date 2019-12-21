8904a36e880fdb9d396489dd2e375967b9022b0e
lakshminarasimmanvenkatakrishnan <lakshmi.narasimman.venkatakrishnan@rabobank.nl>
2019-12-21 20:51:18 +0100

    my first commit

---
+++ src/com/java/solutions/Lexicographical.java
@@ -0,0 +1,18 @@
+package com.java.solutions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+/* Sorting string in Lexicographical order  */
+
+public class Lexicographical {
+
+    /* The program will produce output as following "Hac->Hack->Hacker->HackerRank"  */
+
+    public static void main(String[] args) {
+        String[] words = { "HackerRank", "Hack", "Hacker", "Hac" };
+        Comparator<String> nameComparator = (s1, s2) -> s1.compareTo(s2);
+        System.out.println(Arrays.stream(words).sorted(nameComparator).collect(Collectors.joining("->")));
+    }
+}
