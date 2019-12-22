3fb8c1240c636febdc6d62d58782fb0d35ee6968
lakshminarasimmanvenkatakrishnan <lakshmi.narasimman.venkatakrishnan@rabobank.nl>
2019-12-22 11:40:11 +0100

    Best pair of movies to watch within flying time solution

---
+++ src/com/hackerrank/java/solutions/AverageFinderGroupBy.java
@@ -0,0 +1,26 @@
+package com.hackerrank.java.solutions;
+
+import java.util.Arrays;
+
+import static java.lang.Integer.parseInt;
+import static java.util.Comparator.naturalOrder;
+import static java.util.stream.Collectors.averagingInt;
+import static java.util.stream.Collectors.groupingBy;
+
+/* Finding the average score from the list grouping by candidate name  */
+
+public class AverageFinderGroupBy {
+    public static void main(String[] args) {
+
+        String[][] scores = { { "Simma", "70" }, { "Raghu", "60" }, { "Lakshmi", "70" }, { "Lakshmi", "100" },{ "Raghu", "100" } };
+
+        double highestAvg = Arrays.stream(scores)
+                                  .collect(groupingBy(s -> s[0], averagingInt(s -> parseInt(s[1]))))
+                                  .values()
+                                  .stream()
+                                  .max(naturalOrder())
+                                  .get();
+
+        System.out.println(highestAvg);
+    }
+}
